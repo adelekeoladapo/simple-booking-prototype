949f74762e24dc62c868473c6fb6b8a20990ca15
Dapo Adeleke <adelekeoladapo@gmail.com>
2022-09-08 15:51:02 +0300

    Update config

--- booking-frontend/src/app.d.ts
+++ booking-frontend/src/app.d.ts
@@ -5,4 +5,13 @@ declare namespace App {
 	// interface Locals {}
 	// interface PageData {}
 	// interface Platform {}
+    interface Platform {
+       env: {
+               COUNTER: DurableObjectNamespace;
+           };
+       context: {
+               waitUntil(promise: Promise<any>): void;
+           };
+       caches: CacheStorage & { default: Cache }
+   }
 }
